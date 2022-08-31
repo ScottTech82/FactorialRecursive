a555f97d8b92c349611a0b6d6fc99eaea765fb78
Josh Scott <joshscott82@gmail.com>
2022-08-30 20:22:12 -0400

    Add project files.

---
+++ FactorialTut2/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Factorial with Recursion");
+
+//Factorial Recursively
+
+int target = 6;
+int product = Factorial(target);
+
+int Factorial(int nbr)
+    {
+        if (nbr == 1)
+        {
+            return 1;
+        }
+        return nbr * Factorial(nbr - 1);
+    }
+
+//Factorial Loop not recursive
+
+//int FactorialLoop(int nbr)
+//    {
+//        int product = 1;
+//
+//        for(int i = 1; i <= target; i++)
+//    {
+//        product *= i;
+//    }
+//        return product;
+//    }
+
+    Console.WriteLine($"{target}! is {product}");
+}
